4bc4d7ccb1a7459d9c63a1002670fa966ac4af66
kenya ohara <bureibu20@gmail.com>
2021-10-04 18:58:39 +0900

    index_icon_add

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -8,7 +8,6 @@ class ArticlesController < ApplicationController
     @articles = Article.all
     @rank_articles = Article.order(impressions_count: 'DESC')
     @articles = current_user.articles.search_title(params[:search_title]).page(params[:page]) if params[:search_title].present? 
-    
   end
 
   # GET /articles/1 or /articles/1.json
